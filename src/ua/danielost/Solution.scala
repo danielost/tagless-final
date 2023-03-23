6bab1550d7d948fc3453a1dc4f872d95b1d8e244
Daniel <danya.ostap@gmail.com>
2023-03-23 18:07:03 +0200

    Added the tagless solution

--- src/ua/danielost/Solution.scala
+++ src/ua/danielost/Solution.scala
@@ -3,23 +3,23 @@ package ua.danielost
 object Solution {
 
   object ExpressionProblem {
-    trait Expr
-    case class B(boolean: Boolean) extends Expr
-    case class And(a: Expr, b: Expr) extends Expr
-    case class Or(a: Expr, b: Expr) extends Expr
-    case class Not(expr: Expr) extends Expr
-
-    val exprTest: Expr = Or(And(B(true), B(false)), B(false))
-
-    def eval(expr: Expr): Boolean = expr match {
-      case B(b) => b
-      case And(a, b) => eval(a) && eval(b)
-      case Or(a, b) => eval(a) || eval(b)
-      case Not(a) => !eval(a)
-    }
-
-    case class I(int: Int) extends Expr
-    case class Sum(a: Expr, b: Expr) extends Expr
+//    trait Expr
+//    case class B(boolean: Boolean) extends Expr
+//    case class And(a: Expr, b: Expr) extends Expr
+//    case class Or(a: Expr, b: Expr) extends Expr
+//    case class Not(expr: Expr) extends Expr
+//
+//    val exprTest: Expr = Or(And(B(true), B(false)), B(false))
+//
+//    def eval(expr: Expr): Boolean = expr match {
+//      case B(b) => b
+//      case And(a, b) => eval(a) && eval(b)
+//      case Or(a, b) => eval(a) || eval(b)
+//      case Not(a) => !eval(a)
+//    }
+//
+//    case class I(int: Int) extends Expr
+//    case class Sum(a: Expr, b: Expr) extends Expr
 
     // Solution with tags (the bad one)
     object TagSolution {
@@ -44,15 +44,39 @@ object Solution {
       def eval(expr: Expr): Any = expr match {
         case B(b) => b
         case Or(a, b) => eval(a).asInstanceOf[Boolean] || eval(b).asInstanceOf[Boolean]
-//        same for the other cases
-//        case Or(a, b) => eval(a) || eval(b)
-//        case Not(a) => !eval(a)
+//      same for the other cases
       }
     }
+
+    object TaglessSolution {
+      trait Expr[T]
+      case class B(boolean: Boolean) extends Expr[Boolean]
+      case class And(a: Expr[Boolean], b: Expr[Boolean]) extends Expr[Boolean]
+      case class Or(a: Expr[Boolean], b: Expr[Boolean]) extends Expr[Boolean]
+      case class Not(expr: Expr[Boolean]) extends Expr[Boolean]
+      case class I(int: Int) extends Expr[Int]
+      case class Sum(a: Expr[Int], b: Expr[Int]) extends Expr[Int]
+
+      def eval[T](expr: Expr[T]): T = expr match {
+        case B(b) => b.asInstanceOf[T]
+        case I(i) => i.asInstanceOf[T]
+        case Or(a, b) => (eval(a) || eval(b)).asInstanceOf[T]
+        case Sum(a, b) => (eval(a) + eval(b)).asInstanceOf[T]
+        // same for the other cases
+      }
+    }
+
+    def demoTagless(): Unit = {
+      import TaglessSolution._
+      println(eval(Or(B(true), B(true))))
+      println(eval(Sum(I(15), I(-5))))
+    }
+
+
   }
 
   def main(args: Array[String]) : Unit = {
-
+    import ExpressionProblem._
+    demoTagless()
   }
-
 }
